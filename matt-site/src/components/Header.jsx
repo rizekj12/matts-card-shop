f9058d70ac06f5daa9cc4957899cb00538ce3e57
Joshua Rizek <joshuarizek@gmail.com>
2022-06-03 19:39:18 -0400

    updating making sure code is working

---
+++ matt-site/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Sass/Header.scss'
+
+const Header = () => {
+    return ( 
+        <>
+        <h1 className="headerText">Matt's Card Sales</h1>
+        </>
+     );
+}
+ 
+export default Header;
